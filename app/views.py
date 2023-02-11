edad21a33555040b0cf8141da336236ff2bfc5a3
Kharma-Nailah Johnson <kharmaannjohnson1@gmail.com>
2023-02-10 19:37:28 -0500

    completed profile page

--- app/views.py
+++ app/views.py
@@ -1,10 +1,16 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+import datetime
 
 
 ###
 # Routing for your application.
 ###
+
+todayDate= datetime.datetime.now()
+def format_date_joined(y,m,d):
+    date_joined=datetime.date(y,m,d)
+    return date_joined.strftime("%B, %Y")
 
 @app.route('/')
 def home():
@@ -16,6 +22,12 @@ def home():
 def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
+
+@app.route("/profile")
+def profile():
+    date_j=format_date_joined(2023,2,9)
+    return render_template('profile.html',value=date_j)
+
 
 
 ###
